2442674ce8e1a3d2e3c0b1db5184ef6385d12e93
MaximIvE <93542875+MaximIvE@users.noreply.github.com>
2022-06-21 15:36:39 +0000

    Deploying to gh-pages from @ MaximIvE/goit-js-hw-11@12f69778a6bab412a7eae474f65844078b96f19f 🚀

--- index.33fa4e74.js
+++
@@ -1,2 +0,0 @@
-console.log("Запустився скрипт в index.js. Все добре!");
-//# sourceMappingURL=index.33fa4e74.js.map
--- index.8f2562bd.js
+++
@@ -1,2 +0,0 @@
-console.log("Запустився скрипт в index.js. Все добре!");
-//# sourceMappingURL=index.8f2562bd.js.map
