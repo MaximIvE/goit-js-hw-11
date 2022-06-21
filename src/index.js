7d691f1486049e32c023af8e4e91548e84e495a5
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-21 16:38:33 +0300

    adds a request to the server and generates a card gallery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-console.log("Запустився скрипт в index.js. Все добре!");
+import axios from "axios";
+import createCard from "../../goit-js-hw-10/src/createCard";
+
+const refs = {
+form: document.querySelector('.search-form'),
+galleryEl: document.querySelector('.gallery'),
+};
+refs.form[0].value = 'cat';
+refs.form.addEventListener('submit', onSearch)
+
+async function onSearch(e){
+    e.preventDefault();
+    // e.currentTarget.elements.searchQuery.value = 'cat';
+    const searchQuery = e.currentTarget.elements.searchQuery.value;
+    const answer = await getAnswer(searchQuery);
+    const gallery = await createGallery(answer);
+    refs.galleryEl.insertAdjacentHTML("beforeend", gallery);
+
+    // console.log(gallery);
+
+    
+    // console.log(answer[0]);
+}
+
+async function getAnswer(searchQuery) {
+    const KEY = '28160645-02600786ca706ffa5b60b520e';
+    const url = `https://pixabay.com/api/?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+  
+    try {
+    const response = await axios.get(url);
+    return response.data.hits;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function createGallery(arr){
+    return  arr.reduce((contanier, card) => {
+        contanier = contanier + createOneCard(card);
+        return contanier;
+    }, "");
+}
+
+function createOneCard(card){
+    const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = card;
+    return `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>${downloads}
+      </p>
+    </div>
+  </div>`
+};
